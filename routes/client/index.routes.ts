1a1f0e4a749a3f3eae5e9de2eddc5cae0819212d
Huste6 <quannghihai1@gmail.com>
2024-08-03 22:41:18 +0700

    làm trang danh sách bài hát bên client

--- routes/client/index.routes.ts
+++ routes/client/index.routes.ts
@@ -1,10 +1,10 @@
 import { Express } from "express";
 import {topicRoutes} from "./topic.routes"
+import { songRoutes } from "./song.routes";
 
 const clientRoutes = (app: Express):void => {
-
     app.use('/topics',topicRoutes);
-
+    app.use('/songs',songRoutes);
 }
 
 export default clientRoutes;
