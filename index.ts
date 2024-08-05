4634d092951968d746e4641c1f59b3385152fe23
Huste6 <quannghihai1@gmail.com>
2024-08-05 10:07:57 +0700

    layout trang dashboard (admin)

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import express,{ Express,Request,Response } from "express";
 import dotenv from "dotenv"
 import * as database from "./config/database"
 import clientRoutes from "./routes/client/index.routes";
+import adminRoutes from "./routes/admin/index.routes";
+import { systemConfig } from "./config/config";
+import path from "path"
 
 dotenv.config();
 database.connect();
@@ -13,7 +16,18 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static('public'));
 
+// tinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// end tinyMCE
+
+// app local variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+//client route
 clientRoutes(app);
+
+//admin route
+adminRoutes(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
