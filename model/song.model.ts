861740b5564f052c689e01c35514b43e57d69fe2
Huste6 <quannghihai1@gmail.com>
2024-08-05 14:41:22 +0700

    trang danh sách bài hát

--- model/song.model.ts
+++ model/song.model.ts
@@ -7,6 +7,7 @@ const songSchema = new mongoose.Schema(
         description:String,
         singerId:String,
         topicId:String,
+        status:String,
         like:{
             type: Number,
             default: 0
--- routes/admin/index.routes.ts
+++ routes/admin/index.routes.ts
@@ -2,12 +2,14 @@ import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.routes";
 import { systemConfig } from "../../config/config";
 import { topicsRoutes } from "./topic.routes";
+import { songRoutes } from "./song.routes";
 
 const adminRoutes = (app: Express):void => {
     const PATH_ADMIN = `${systemConfig.prefixAdmin}`;
 
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes);
     app.use(`${PATH_ADMIN}/topics`, topicsRoutes);
+    app.use(`${PATH_ADMIN}/songs`, songRoutes);
 }
 
 export default adminRoutes;
