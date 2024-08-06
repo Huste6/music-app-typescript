7d558f8c2b7ef3ff9c739fd00b52d59c8c6dcdb6
Huste6 <quannghihai1@gmail.com>
2024-08-06 11:03:06 +0700

    Nghe thử audio trcs khi create

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -20,3 +20,20 @@ if(uploadImage){
     // })
 }
 // end upload image
+
+// upload-audio-play
+const uploadAudio = document.querySelector("[upload-audio]");
+if(uploadAudio){
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]")
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e)=>{
+        if(e.target.files.length){
+            const audio = URL.createObjectURL(e.target.files[0]);
+            source.src = audio;
+            uploadAudioPlay.load();
+        }
+    })
+} 
+// end upload-audio-play
